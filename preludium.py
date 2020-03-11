2c54dd07f10b545b8c8e7cbe2ec7a22020bf1882
Adam Wawrzeńczyk <adam.wawrzenczyk@gmail.com>
2020-03-11 10:08:37 +0100

    Basic preludium plots

--- preludium.py
+++ preludium.py
@@ -8,7 +8,19 @@ df = pd.read_csv('data/preludium.csv')
 df
 
 # %%
+sns.boxplot(df.edition, df.budget)
+
+# %%
+sns.boxplot(df.loc[(df.edition != 1) & (df.edition != 2), :].edition, df.budget)
+
+# %%
 plt.hist(df.edition, bins=len(np.unique(df.edition)),\
     rwidth=0.85)
+
+# %%
+count_by_edition = df.groupby(df.edition).size()\
+    .reset_index(drop = False)\
+    .rename(columns = { 0: 'count' })
+sns.barplot(count_by_edition.edition, count_by_edition['count'])
 
 # %%
