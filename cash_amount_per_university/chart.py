3cb087670eb8800b1377e7c085c5ba01007645a9
Ania Szymanska <aejszymanska@gmail.com>
2020-04-01 12:47:16 +0200

    change style of cash amount per university

--- cash_amount_per_university/chart.py
+++ cash_amount_per_university/chart.py
@@ -4,18 +4,32 @@ import seaborn as sns
 
 df = pd.read_csv('../data/preludium_with_additional_info_cleared.csv')
 
-sums = df\
-    .groupby(['unit'])['budget']\
-    .sum()\
-    .reset_index()\
-    .sort_values(by = ['budget'], ascending=False)\
-    .head(10)\
+sums = df \
+    .groupby(['unit'])['budget'] \
+    .sum() \
     .reset_index() \
+    .sort_values(by=['budget'], ascending=False) \
+    .head(10) \
+    .reset_index()
 
 sums['budget'] = sums['budget'].div(1000000)
-
-sns.barplot(x='budget', y='unit', data=sums)
+sns.set_style("whitegrid")
+sns.barplot(x='budget', y='unit', data=sums, color='#e12647')
 plt.ylabel('Jednostka')
 plt.xlabel('Suma grantów [mln zł]')
-plt.title('Jednostki o najwyższej sumie przydzielonych grantów')
-plt.savefig('count_by_university.svg',format='svg', bbox_inches='tight')
+
+# title = 'Jednostki o najwyższej sumie przydzielonych grantów'
+# plt.setp(title, color='#545454')
+
+ax = plt.gca()
+ax.spines['bottom'].set_visible(False)
+ax.spines['left'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.xaxis.label.set_color('#545454')
+ax.yaxis.label.set_color('#545454')
+ax.tick_params(axis='x', colors='#545454')
+ax.tick_params(axis='y', colors='#545454')
+
+plt.savefig('count_by_university.svg', format='svg', bbox_inches='tight', transparent=True)
+plt.savefig('count_by_university.png', format='png', bbox_inches='tight', transparent=True, dpi=800)
